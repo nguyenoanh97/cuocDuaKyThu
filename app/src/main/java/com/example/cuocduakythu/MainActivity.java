623c5596040493b72c11f77b1278e3ca28acc65b
oanh0444@gmail.com <anhnguyetsol97@gmail.com>
2021-04-26 18:13:16 +0700

    Oanh: lan 2

--- app/src/main/java/com/example/cuocduakythu/MainActivity.java
+++ app/src/main/java/com/example/cuocduakythu/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.cuocduakythu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtDiem;
+    CheckBox cbOne, cbTwwo, cbThree;
+    SeekBar skOne, skTwo, skThree;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
