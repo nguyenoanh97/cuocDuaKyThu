7b72997e5005c617249442bef5be6bdbf1ffcd54
oanh0444@gmail.com <anhnguyetsol97@gmail.com>
2021-04-28 15:15:08 +0700

    lan 5

--- app/src/main/java/com/example/cuocduakythu/MainActivity.java
+++ app/src/main/java/com/example/cuocduakythu/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     CheckBox cbOne, cbTwo, cbThree;
     SeekBar skOne, skTwo, skThree;
     ImageButton ibtnPlay;
+    int soDiem = 100;
 
 
     @Override
@@ -27,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AnhXa();
+        // Không cho tương tác seekbar
+        skOne.setEnabled(false);
+        skTwo.setEnabled(false);
+        skThree.setEnabled(false);
+
         CountDownTimer countDownTimer = new CountDownTimer(20000, 300) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -40,6 +46,17 @@ public class MainActivity extends AppCompatActivity {
                     ibtnPlay.setVisibility(View.VISIBLE);
                     this.cancel();
                     Toast.makeText(MainActivity.this, "One Win", Toast.LENGTH_SHORT).show();
+                    if(cbOne.isChecked()){
+                        soDiem += 10;
+                        Toast.makeText(MainActivity.this, "Bạn đoán chính xác", Toast.LENGTH_SHORT).show();
+
+                    }else {
+                        soDiem -= 5;
+                        Toast.makeText(MainActivity.this, "Bạn đoán sai rồi", Toast.LENGTH_SHORT).show();
+
+                    }
+                    txtDiem.setText(soDiem + "");
+                    EnableCheckBox();
                 }
                 // kiểm tra Win two
                 if(skTwo.getProgress() >= skTwo.getMax()) {
@@ -47,6 +64,15 @@ public class MainActivity extends AppCompatActivity {
 
                     this.cancel();
                     Toast.makeText(MainActivity.this, "Two Win", Toast.LENGTH_SHORT).show();
+                    if(cbTwo.isChecked()){
+                        soDiem += 10;
+                        Toast.makeText(MainActivity.this, "Bạn đoán chính xác", Toast.LENGTH_SHORT).show();
+                    }else {
+                        soDiem -= 5;
+                        Toast.makeText(MainActivity.this, "Bạn đoán sai rồi", Toast.LENGTH_SHORT).show();
+                    }
+                    txtDiem.setText(soDiem + "");
+                    EnableCheckBox();
                 }
                 // kiểm tra Win three
                 if(skThree.getProgress() >= skThree.getMax()) {
@@ -54,6 +80,17 @@ public class MainActivity extends AppCompatActivity {
 
                     this.cancel();
                     Toast.makeText(MainActivity.this, "Three Win", Toast.LENGTH_SHORT).show();
+                    if(cbThree.isChecked()){
+                        soDiem += 10;
+                        Toast.makeText(MainActivity.this, "Bạn đoán chính xác", Toast.LENGTH_SHORT).show();
+
+                    }else {
+                        soDiem -= 5;
+                        Toast.makeText(MainActivity.this, "Bạn đoán sai rồi", Toast.LENGTH_SHORT).show();
+
+                    }
+                    txtDiem.setText(soDiem + "");
+                    EnableCheckBox();
                 }
 
                 skOne.setProgress(skOne.getProgress()+ one);
@@ -70,24 +107,58 @@ public class MainActivity extends AppCompatActivity {
         ibtnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(cbOne.isChecked() || cbTwo.isChecked() || cbThree.isChecked()){
+
                 skOne.setProgress(0);
                 skTwo.setProgress(0);
                 skThree.setProgress(0);
-
                 ibtnPlay.setVisibility(View.INVISIBLE);
-                countDownTimer.start();
+                    countDownTimer.start();
+                    DisableCheckBox();
+             }else{
+                    Toast.makeText(MainActivity.this, "Vui lòng đặt cược trước khi chơi", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
+//Không cho tương tác checkbox khi đang chạy
         cbOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                cbTwo.setChecked(false);
+                cbThree.setChecked(false);
+
+            }
+        });
+        cbTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                cbOne.setChecked(false);
+                cbThree.setChecked(false);
+
+            }
+        });
+        cbThree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                cbTwo.setChecked(false);
+                cbOne.setChecked(false);
 
             }
         });
 
 
     }
+    private void EnableCheckBox() {
+        cbOne.setEnabled(true);
+        cbTwo.setEnabled(true);
+        cbThree.setEnabled(true);
+    }
+    private void DisableCheckBox() {
+        cbOne.setEnabled(false);
+        cbTwo.setEnabled(false);
+        cbThree.setEnabled(false);
+    }
+
     private void AnhXa() {
         txtDiem  = (TextView) findViewById(R.id.textviewDiemSo);
         cbOne    = (CheckBox) findViewById(R.id.checkboxOne);
